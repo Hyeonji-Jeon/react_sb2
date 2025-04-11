22265d0f42a679ce607b541dc21b2d6e2d7f2c1b
Hyeonji Jeon <guswl1397@naver.com>
2025-04-11 14:31:28 +0900

    todo

--- src/main/java/org/zerock/sb2/todo/controller/TodoController.java
+++ src/main/java/org/zerock/sb2/todo/controller/TodoController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.sb2.todo.dto.ActionResultDTO;
 import org.zerock.sb2.todo.dto.TodoDTO;
 
 import java.util.List;
@@ -33,7 +35,27 @@ public class TodoController {
                         .build()
                 );
 
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+
         return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ActionResultDTO<Long>> post(TodoDTO dto){
+
+        log.info("---------post----------");
+        log.info(dto);
+
+        return ResponseEntity.ok(ActionResultDTO.<Long>builder()
+                        .result("success")
+                        .data(10L)
+                .build());
+
     }
 
 }
---
+++ src/main/java/org/zerock/sb2/todo/dto/ActionResultDTO.java
@@ -0,0 +1,18 @@
+package org.zerock.sb2.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActionResultDTO<T> {
+
+    private String result;
+
+    private T data;
+
+}
