f58be8a9aa4270b5a35dec835ee33f7cdaf0ddaf
Hyeonji Jeon <guswl1397@naver.com>
2025-04-11 13:42:40 +0900

    setting

---
+++ src/main/java/org/zerock/sb2/config/CustomSecurityConfig.java
@@ -0,0 +1,49 @@
+package org.zerock.sb2.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Log4j2
+@Configuration
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        log.info("--------configure---------");
+
+        http.formLogin(config -> {
+            config.disable();
+        });
+
+        http.sessionManagement(config -> {
+           config.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+}
