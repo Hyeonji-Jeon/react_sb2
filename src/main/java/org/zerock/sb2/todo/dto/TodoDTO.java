09de99d3762104db2c4ab1496165dba3b01c5e3a
Hyeonji Jeon <guswl1397@naver.com>
2025-04-15 13:07:19 +0900

    todo

--- src/main/java/org/zerock/sb2/todo/dto/TodoDTO.java
+++ src/main/java/org/zerock/sb2/todo/dto/TodoDTO.java
@@ -1,17 +1,21 @@
 package org.zerock.sb2.todo.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 
 @NoArgsConstructor
 @Builder
 public class TodoDTO {
-  
+
   private Long tno;
 
   private String title;
@@ -22,6 +26,8 @@ public class TodoDTO {
 
   private LocalDateTime modDate;
 
+  private List<MultipartFile> file = new ArrayList<>();
+
   public TodoDTO(Long tno, String title, String writer, LocalDateTime regDate, LocalDateTime modDate) {
     this.tno = tno;
     this.title = title;
@@ -30,7 +36,12 @@ public class TodoDTO {
     this.modDate = modDate;
   }
 
-
-  
-
+  public TodoDTO(Long tno, String title, String writer, LocalDateTime regDate, LocalDateTime modDate, List<MultipartFile> file) {
+    this.tno = tno;
+    this.title = title;
+    this.writer = writer;
+    this.regDate = regDate;
+    this.modDate = modDate;
+    this.file = file;
+  }
 }
