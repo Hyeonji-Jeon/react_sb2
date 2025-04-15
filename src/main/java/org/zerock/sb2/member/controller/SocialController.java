e17d99605183c40e117bfa2af857f344e6357696
Hyeonji Jeon <guswl1397@naver.com>
2025-04-15 15:42:21 +0900

    kakao

--- src/main/java/org/zerock/sb2/member/controller/SocialController.java
+++ src/main/java/org/zerock/sb2/member/controller/SocialController.java
@@ -2,6 +2,7 @@ package org.zerock.sb2.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SocialController {
 
     @RequestMapping("/api/v1/member/kakao")
-    public ResponseEntity<String[]> getKakao(@RequestParam("accessToken") String accessToken) {
+    public ResponseEntity<String[]> getKakao( @RequestParam("accessToken") String accessToken) {
 
-        log.info("getKako: " + accessToken);
+        log.info("getKakao: " + accessToken);
 
-        String[] result = new String[]{"access...........", "refresh..............."};
+        String[] result = new String[]{"access.........", "refresh............."};
 
         return ResponseEntity.ok(result);
+
     }
 }
