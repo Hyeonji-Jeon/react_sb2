f1b59b0649098a7686c5fca720f36d75ef06a939
Hyeonji Jeon <guswl1397@naver.com>
2025-04-16 13:20:46 +0900

    JWT

--- src/main/java/org/zerock/sb2/member/filter/JWTCheckFilter.java
+++ src/main/java/org/zerock/sb2/member/filter/JWTCheckFilter.java
@@ -21,7 +21,10 @@ public class JWTCheckFilter extends OncePerRequestFilter {
 
     public enum JWTErrorCode {
 
-        NO_ACCESS_TOKEN(401, "No access token");
+        NO_ACCESS_TOKEN(401, "No access token"),
+        EXPIRED_TOKEN(401, "Expired token"),
+        BAD_SIGNATURE(401, "Bad signature"),
+        MALFORMED_TOKEN(401, "Malformed token");
 
         private int code;
         private String message;
@@ -71,8 +74,15 @@ public class JWTCheckFilter extends OncePerRequestFilter {
         try{
             jwtUtil.validateToken(accessToken);
         }catch(Exception e){
-            log.error("======================");
-            log.error(e.getMessage());
+            log.error("===========================");
+            String message = e.getMessage();
+            if(message.startsWith("JWT signature")){
+                handleException(response, JWTErrorCode.BAD_SIGNATURE);
+            }else if(message.startsWith("Malformed")){
+                handleException(response, JWTErrorCode.MALFORMED_TOKEN);
+            }else if(message.startsWith("JWT expired")){
+                handleException(response, JWTErrorCode.EXPIRED_TOKEN);
+            }
         }
 
         filterChain.doFilter(request, response);
