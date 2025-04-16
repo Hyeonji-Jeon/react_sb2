988dd09ac4b85828180886d721dfa1c6944d588a
Hyeonji Jeon <guswl1397@naver.com>
2025-04-16 11:21:21 +0900

    JWT

---
+++ src/main/java/org/zerock/sb2/member/filter/JWTCheckFilter.java
@@ -0,0 +1,36 @@
+package org.zerock.sb2.member.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.sb2.util.JWTUtil;
+
+import java.io.IOException;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class JWTCheckFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+
+        log.info("------shouldNotFilter------------");
+
+        return false;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        log.info("------doFilterInternal------------");
+
+    }
+}
